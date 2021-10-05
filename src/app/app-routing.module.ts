7c77ac0fb10c2d736c10e27c412f3e0b5259200a
TheLegend27-ger <55186709+TheLegend27-ger@users.noreply.github.com>
2021-10-05 12:09:00 +0200

    category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,20 +11,13 @@ import { OneQuestionComponent } from './components/quiz/one-question/one-questio
 import { DataUploaderTipsEditorComponent } from './components/data-uploader/data-uploader-tips-editor/data-uploader-tips-editor.component';
 import { DataUploaderQuestionsEditorComponent } from './components/data-uploader/data-uploader-questions-editor/data-uploader-questions-editor.component';
 import { Category1Component } from './components/tips/all-tips/category1/category1.component';
-import { Category2Component } from './components/tips/all-tips/category2/category2.component';
-import { Category3Component } from './components/tips/all-tips/category3/category3.component';
-import { Category4Component } from './components/tips/all-tips/category4/category4.component';
-import { Category5Component } from './components/tips/all-tips/category5/category5.component';
+
 
 
 const routes: Routes = [
   { path: '', component: SomeTipsComponent },
   { path: 'alltips', component: AllTipsComponent },
   { path: 'alltips/category1', component: Category1Component },
-  { path: 'alltips/category2', component: Category2Component },
-  { path: 'alltips/category3', component: Category3Component },
-  { path: 'alltips/category4', component: Category4Component },
-  { path: 'alltips/category5', component: Category5Component },
   { path: 'data', component: DataUploaderComponent },
   { path: 'edittip/:tipId', component: DataUploaderTipsEditorComponent },
   { path: 'edittip', component: DataUploaderTipsEditorComponent },
